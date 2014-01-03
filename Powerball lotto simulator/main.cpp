181ed0f87ebf2c281044e48f4ec827168f94bc2e
Diogo Martinho <DiogoMartinho@mbp-de-diogo.lan>
2014-01-03 20:00:00 +0000

    Initial Commit

---
+++ Powerball lotto simulator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Powerball lotto simulator
+//
+//  Created by Diogo Martinho on 03/01/14.
+//  Copyright (c) 2014 Diogo Martinho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
